198e6518be0935c9b2ac4f8e3d85073e3bf39af7
Ed <ed@polarity.io>
2023-01-16 15:17:46 -0500

    Finish throttling

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# Polarity Criminal IP  Integration
+# Polarity Criminal IP Integration
 
-The Polarity Criminal IP integration searches returns information from Criminal IP on IPv4 addresses.
+The Polarity Criminal IP integration returns information from Criminal IP on IPv4 addresses.
+
+## Search Throttling
+
+The Criminal IP REST API only allows 1 outstanding search at a time per API key.  As a result, searches to the integration will be run in order serially.  The integration will queue up to 15 searches at a time per API key in use.  Once the search queue is filled, Polarity will
+return a message in the Overlay Window indicating the search limit has been reached and provide an option to retry the search.
+
+<img src="images/overlay.png" width="50%">
 
 ## Integration Options
 
@@ -13,6 +20,6 @@ Installation instructions for integrations are provided on the [PolarityIO GitHu
 
 ## Polarity
 
-Polarity is a memory-augmentation platform that improves and accelerates analyst decision making.  For more information about the Polarity platform please see:
+Polarity is a memory-augmentation platform that improves and accelerates analyst decision-making.  For more information about the Polarity platform please see:
 
 https://polarity.io/
--- integration.js
+++ integration.js
@@ -1,76 +1,138 @@
-const async = require('async');
-const request = require('request');
 const _ = require('lodash');
+const Bottleneck = require('bottleneck');
+const cache = require('memory-cache');
+const { setLogger } = require('./src/logger');
+const { ApiRequestError, parseErrorToReadableJSON } = require('./src/errors');
+const request = require('./src/polarity-request');
 
 let Logger;
+const IGNORED_IPS = new Set(['127.0.0.1', '255.255.255.255', '0.0.0.0']);
+const MAX_DOMAINS = 250;
+const MAX_LOOKUP_QUEUE_SIZE_PER_API_KEY = 15;
+const bottlneckApiKeyCache = new cache.Cache();
 
 function startup(logger) {
   Logger = logger;
+  setLogger(Logger);
 }
 
-function doLookup(entities, options, cb) {
-  const lookupResults = [];
-  async.each(
-    entities,
-    (entity, done) => {
-      const requestOptions = {
-        uri: 'https://api.criminalip.io/v1/ip/data',
-        qs: {
-          ip: entity.value,
-          full: true
-        },
-        headers: {
-          'x-api-key': options.apiKey
-        },
-        json: true
-      };
+async function doLookup(entities, options, cb) {
+  try {
+    Logger.trace({ entities }, 'doLookup');
+    const lookupResults = [];
 
-      request(requestOptions, (err, response, body) => {
-        if (err) {
-          return done({
-            detail: 'HTTP Error',
-            err
-          });
-        }
+    let limiter = bottlneckApiKeyCache.get(options.apiKey);
 
-        //Logger.trace({ body, status: response.statusCode }, 'Lookup response body');
+    if (!limiter) {
+      limiter = new Bottleneck({
+        id: options.apiKey,
+        maxConcurrent: 1,
+        highWater: MAX_LOOKUP_QUEUE_SIZE_PER_API_KEY,
+        strategy: Bottleneck.strategy.OVERFLOW
+      });
+      bottlneckApiKeyCache.put(options.apiKey, limiter);
+    }
 
-        if (body.status === 200) {
-          lookupResults.push({
+    const tasks = entities.map(async (entity) => {
+      try {
+        return await limiter.schedule(() => lookupIp(entity, options));
+      } catch (limiterError) {
+        if (limiterError instanceof Bottleneck.BottleneckError) {
+          return {
             entity,
             data: {
-              summary: getSummaryTags(body),
+              summary: ['Search Limit Reached'],
               details: {
-                // Select data to send to template
-                tags: body.tags,
-                inboundScore: scoreToHumanReadable(body.score.inbound),
-                outboundScore: scoreToHumanReadable(body.score.outbound),
-                ip_category: getUniqueCategories(body),
-                domain: body.domain,
-                portSummary: createPortSummary(body)
+                apiLimitReached: true
               }
             }
-          });
-          done();
+          };
         } else {
-          Logger.error(
-            { status: body.status, body },
-            'Unexpected HTTP Status Code Received'
-          );
-          done({
-            detail: body.message
-              ? body.message
-              : `Unexpected HTTP Status Code Received: ${body.status}`,
-            body
-          });
+          throw limiterError;
         }
-      });
+      }
+    });
+    const results = await Promise.all(tasks);
+
+    //Logger.info({ results }, 'lookup results');
+
+    results.forEach((result) => {
+      lookupResults.push(result);
+    });
+
+    cb(null, lookupResults);
+  } catch (error) {
+    const errorAsPojo = parseErrorToReadableJSON(error);
+    Logger.error({ error: errorAsPojo }, 'Error in doLookup');
+    return cb(errorAsPojo);
+  }
+}
+
+async function lookupIp(entity, options) {
+  if (!isValidIP(entity)) {
+    return {
+      entity,
+      data: null
+    };
+  }
+
+  const requestOptions = {
+    uri: 'https://api.criminalip.io/v1/ip/data',
+    qs: {
+      ip: entity.value,
+      full: true
     },
-    (err) => {
-      Logger.trace({ lookupResults }, 'Lookup Results');
-      cb(err, lookupResults);
+    headers: {
+      'x-api-key': options.apiKey
+    },
+    json: true
+  };
+
+  const { body, statusCode } = await request.request(requestOptions);
+
+  Logger.trace({ body, status: statusCode }, 'Lookup response body');
+
+  // Note, the criminalIP API does not use the HTTP statusCode to signal errors.  The HTTP status code is
+  // always 200 and then they set the "status" property on the returned payload (body), to the actual
+  // status code.
+  if (body.status === 200) {
+    if (body.domain && Array.isArray(body.domain.data)) {
+      body.domain.totalResults = body.domain.data.length;
+      body.domain.data = body.domain.data.slice(0, MAX_DOMAINS);
+      body.domain.isTruncated = true;
     }
-  );
+
+    return {
+      entity,
+      data: {
+        summary: getSummaryTags(body),
+        details: {
+          // Select data to send to template
+          tags: body.tags,
+          inboundScore: scoreToHumanReadable(body.score.inbound),
+          outboundScore: scoreToHumanReadable(body.score.outbound),
+          ip_category: getUniqueCategories(body),
+          domain: body.domain,
+          portSummary: createPortSummary(body)
+        }
+      }
+    };
+  } else {
+    // Note, the criminalIP API does not use the HTTP statusCode to signal errors.  The HTTP status code is
+    // always 200 and then they set the "status" property on the returned payload (body), to the actual
+    // status code.
+    Logger.error({ status: body.status, body }, 'Unexpected HTTP Status Code Received');
+    throw new ApiRequestError(
+      body.message
+        ? body.message
+        : `Unexpected status code ${body.status} received when making request to CriminalIP API`,
+      {
+        body,
+        statusCode,
+        requestOptions
+      }
+    );
+  }
 }
 
 function getSummaryTags(body) {
@@ -112,12 +174,29 @@ function scoreToHumanReadable(score) {
 }
 
 /**
+ * Ignore Private IPs, as well as IPs in the range 0.0.0.0/8 (CriminalIP will return a 400 error for these
+ * as it does not consider them valid).
+ * @param entity
+ * @returns {boolean}
+ */
+function isValidIP(entity) {
+  if (
+    entity.isPrivateIP ||
+    IGNORED_IPS.has(entity.value) ||
+    entity.value.startsWith('0.')
+  ) {
+    return false;
+  }
+  return true;
+}
+
+/**
  * Return an array of unique open port numbers
  * @param body
  */
-function createPortSummary(body){
+function createPortSummary(body) {
   const uniquePorts = new Set();
-  body.port.data.forEach(port => {
+  body.port.data.forEach((port) => {
     uniquePorts.add(port.open_port_no);
   });
   return Array.from(uniquePorts);
@@ -132,7 +211,30 @@ function getUniqueCategories(body) {
   }, []);
 }
 
+function retrySearch({ data: { entity } }, options, cb) {
+  Logger.trace({ entity }, 'onMessage Entity');
+  doLookup([entity], options, (err, lookupResults) => {
+    if (err) return cb(err);
+    Logger.trace({ lookupResults }, 'onMessage lookup results');
+    const lookupResult = lookupResults[0];
+
+    if (lookupResult && lookupResult.data && lookupResult.data === null) {
+      cb(null, {
+        summary: ['No Results Found'],
+        details: {
+          noResultsFound: true,
+          isRetry: true
+        }
+      });
+    } else {
+      lookupResult.data.details.isRetry = true;
+      cb(null, lookupResult.data);
+    }
+  });
+}
+
 module.exports = {
   startup,
-  doLookup
+  doLookup,
+  onMessage: retrySearch
 };
